9355170bf25e39a208f0ebbe1a19a7e2051d5b48
Pawel Debinski <spokojasne@interia.eu>
2018-10-01 22:29:21 +0200

    init commit

---
+++ src/main/java/pl/pawel/unittesting/UnitTestingMockitoApplication.java
@@ -0,0 +1,12 @@
+package pl.pawel.unittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestingMockitoApplication.class, args);
+    }
+}
