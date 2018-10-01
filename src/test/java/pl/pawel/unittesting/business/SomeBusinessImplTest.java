5839a356ba7c746e576226ae4b970b8c72e2663d
Pawel Debinski <spokojasne@interia.eu>
2018-10-01 22:46:13 +0200

    added simple Junit tests, closes #1

---
+++ src/test/java/pl/pawel/unittesting/business/SomeBusinessImplTest.java
@@ -0,0 +1,32 @@
+package pl.pawel.unittesting.business;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SomeBusinessImplTest {
+
+    @Test
+    public void calculateSum_basic() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResult = business.calculateSum(new int[]{1, 2, 3});
+        int expectedResult = 6;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void calculateSum_emptyArray() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResult = business.calculateSum(new int[]{});
+        int expectedResult = 0;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void calculateSum_oneValue() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResult = business.calculateSum(new int[]{7});
+        int expectedResult = 7;
+        assertEquals(expectedResult, actualResult);
+    }
+}
