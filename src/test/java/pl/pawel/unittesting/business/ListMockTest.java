5bf05f57287038555f31d0e0933c396aba798e58
Pawel Debinski <spokojasne@interia.eu>
2018-10-02 10:51:35 +0200

    mockito spy, closes #10

--- src/test/java/pl/pawel/unittesting/business/ListMockTest.java
+++ src/test/java/pl/pawel/unittesting/business/ListMockTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -75,4 +76,33 @@ public class ListMockTest {
         assertEquals("SomeString1", allValues.get(0));
         assertEquals("SomeString2", allValues.get(1));
     }
+
+    @Test
+    public void mocking() {
+        ArrayList arrayListMock = mock(ArrayList.class);
+        System.out.println(arrayListMock.get(0)); // null
+        System.out.println(arrayListMock.size()); // 0
+        arrayListMock.add("test");
+        arrayListMock.add("test2");
+        System.out.println(arrayListMock.size()); // 0
+        when(arrayListMock.size()).thenReturn(5);
+        System.out.println(arrayListMock.size()); // 5
+    }
+
+    @Test
+    public void spyin() {
+        ArrayList arrayListSpy = spy(ArrayList.class);
+        arrayListSpy.add("test0");
+        System.out.println(arrayListSpy.get(0)); // test0
+        System.out.println(arrayListSpy.size()); // 1
+        arrayListSpy.add("test");
+        arrayListSpy.add("test2");
+        System.out.println(arrayListSpy.size()); // 3
+        when(arrayListSpy.size()).thenReturn(5);
+        System.out.println(arrayListSpy.size()); // 5
+        arrayListSpy.add("test4");
+        System.out.println(arrayListSpy.size()); // 5
+        verify(arrayListSpy).add("test4");
+    }
+
 }
